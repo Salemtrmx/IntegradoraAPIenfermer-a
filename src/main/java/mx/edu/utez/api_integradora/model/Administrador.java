b9e645bdae289365db89a28a7be069fc3fed7074
Salem <20233tn096@utez.edu.mx>
2024-11-28 15:18:05 -0600

    Subo los Models

---
+++ src/main/java/mx/edu/utez/api_integradora/model/Administrador.java
@@ -0,0 +1,57 @@
+package mx.edu.utez.api_integradora.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Administrador")
+public class Administrador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contraseña;
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
---
+++ src/test/java/mx/edu/utez/api_integradora/ApiIntegradoraApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.api_integradora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIntegradoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
